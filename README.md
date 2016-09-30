b24dd68713f1cd62fa5fad5043a5bd91b26b4aac
Neverland <enix@foxmail.com>
2016-09-30 16:51:30 +0800

    complate

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # fin-cli
-fin-cli
+-----------------
+fin-cli  曼哈顿cli
+---------------
+
+### usage
+
+1. fin init 
+   -uri|u git仓库所在的uri
+```javascript
+    fin install -u git.fin.com
+```
--- command/init.js
+++ command/init.js
@@ -7,37 +7,90 @@
 
 'use strict';
 
-const exec = require('child_process').exec;
-const co = require('co');
-const prompt = require('co-prompt');
-const chalk = require('chalk');
+const PROGRAM = require('commander');
+const EXEC = require('child_process').exec;
+const CO = require('co');
+const PROMPT = require('co-prompt');
+const CHALK = require('chalk');
 
 module.exports = () => {
-    co(function *() {
-        let tplName = yield prompt('Template name: ');
-        let projectName = yield prompt('Project name: ');
-        let gitUrl;
-        let branch;
+    CO(function *() {
+        let uri = PROGRAM.args[0].uri;
 
-        if (!config.tpl[tplName]) {
-            console.log(chalk.red('\n × Template does not exit!'));
-            process.exit()
-        }
-        gitUrl = config.tpl[tplName].url;
-        branch = config.tpl[tplName].branch;
-
-        let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`;
-
-        console.log(chalk.white('\n Start generating...'));
-
-        exec(cmdStr, (error, stdout, stderr) => {
-            if (error) {
-                console.log(error);
-                process.exit()
-            }
-            console.log(chalk.green('\n √ Generation completed!'));
-            console.log(`\n cd ${projectName} && npm install \n`);
+        if (!uri) {
+            console.log(CHALK.bold.red('\n × uri does not exit!'));
             process.exit();
-        })
+        }
+
+        let projectName = yield PROMPT('Project name: ');
+
+        if (!projectName) {
+            console.log(CHALK.bold.red('\n × Project name does not exit!'));
+            process.exit();
+        }
+
+        console.log(CHALK.white('\n Start generating...'));
+
+        let cloneInit = () => {
+            let command = `git clone ${uri}/baidu/finland/init ${projectName}`;
+
+            EXEC(command, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(CHALK.bold.red(`\n × Command failed: ${error.cmd}`));
+                    process.exit();
+                }
+                console.log(stderr);
+
+                npmInstall();
+            });
+        };
+
+        let npmInstall = () => {
+            console.log(CHALK.bold.yellow(`\n npm install... \n`));
+
+            EXEC(`cd ${projectName} && npm install --production`,  (error, stdout, stderr) => {
+                if (error) {
+                    console.log(CHALK.bold.red(`\n × ${error}`));
+                    process.exit();
+                }
+                console.log(stdout, stderr);
+
+                addComponent();
+            });
+        };
+
+        let addComponent = () => {
+            let modules = [
+                ['FinanceGeneral', 'fin-fg'],
+                ['UserInterface', 'fin-ui'],
+                ['RiskManagement', 'fin-rm'],
+                ['ResultState', 'fin-rs'],
+                ['InstitutionServices', 'fin-is'],
+                ['FinanceKernel', 'fin-fk'],
+                ['CustomerServices', 'fin-cs']
+            ];
+            let command = [];
+
+            console.log(CHALK.bold.yellow(`\n add submodule... \n`));
+
+            modules.forEach(item => {
+                command.push(`git submodule add ${uri}/baidu/finland/${item[0]} components/${item[1]}`);
+            });
+
+            command = `cd ${projectName} && ${command.join(' && ')}`;
+
+            EXEC(command, (error, stdout, stderr) => {
+                if (error) {
+                    console.log(CHALK.bold.red(`\n × ${error}`));
+                    process.exit();
+                }
+                console.log(stdout, stderr);
+
+                console.log(CHALK.green('\n √ Generation completed!'));
+                process.exit();
+            });
+        };
+
+        cloneInit();
     })
 };
