ff7be1cdd48a0f62c71d9d60d2f616a707514691
ienix <guoaimin01@baidu.com>
2017-03-28 19:22:03 +0800

    update template

--- command/init.js
+++ command/init.js
@@ -128,8 +128,7 @@ module.exports = () => {
             command.push(`cd ${projectName}`);
             command.push(`${publishCommand} build.sh"' > BCLOUD`);
             command.push(`${publishCommand} build-qa.sh"' > BCLOUD.qa`);
-            command.push('git add BCLOUD');
-            command.push('git add BCLOUD.qa');
+            command.push('git add BCLOUD BCLOUD.qa');
             command.push('git commit -m "init finland"');
 
             console.log(CHALK.bold.yellow(`\n create build file... \n`));
